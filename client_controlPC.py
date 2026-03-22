54be3260cb7d3845cf48079d899410ddceacf1db
yoavg222 <yoavg222@gmail.com>
2026-03-23 00:57:10 +0200

    Fixed keyboard function

--- client_controlPC.py
+++ client_controlPC.py
@@ -9,11 +9,42 @@ import time
 SERVER_IP = "192.168.1.126"
 PORT_KEYBOARD_SOCKET = 12346
 
+SIZE_HEADER_FORMAT = "0000000|"
+size_header_size = len(SIZE_HEADER_FORMAT)
+TCP_DEBUG = True
+LEN_TO_PRINT = 100
+
+
+def recv_by_size(sock):
+    size_header = b''
+    data_len = 0
+    while len(size_header) < size_header_size:
+        _s = sock.recv(size_header_size - len(size_header))
+        if _s == b'':
+            size_header = b''
+            break
+        size_header += _s
+    data  = b''
+    if size_header != b'':
+        data_len = int(size_header[:size_header_size - 1])
+        while len(data) < data_len:
+            _d = sock.recv(data_len - len(data))
+            if _d == b'':
+                data  = b''
+                break
+            data += _d
+
+    if  TCP_DEBUG and size_header != b'':
+        print ("\nRecv(%s)>>>" % (size_header,), end = '')
+        print ("%s"%(data[:min(len(data),LEN_TO_PRINT)],))
+    if data_len != len(data):
+        data=b''
+    return data
 
 
 def check_turn_on_button(button):
-    time.sleep(5)
-    pyautogui.click()
+    # time.sleep(5)
+    # pyautogui.click()
     pyautogui.write(button)
     # pyautogui.typewrite(["enter"])
 
@@ -21,23 +52,22 @@ def check_turn_on_button(button):
 def computer_keyboard_client():
     keyboard_socket_client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     keyboard_socket_client.connect((SERVER_IP,PORT_KEYBOARD_SOCKET))
-    try:
-        socket_file = keyboard_socket_client.makefile('r')
 
-    except:
-        return
+    # try:
+    #     socket_file = keyboard_socket_client.makefile('r')
+    # except:
+    #     return
 
-
+    time.sleep(5)
+    pyautogui.click()
     while True:
         try:
-            line = socket_file.readline()
-            if not line:
+            data = recv_by_size(keyboard_socket_client).decode()
+            if not data:
                 break
 
-            data = json.loads(line)
-            print(f"i got: {data['key']}")
-            check_turn_on_button(data['key'])
-
+            print(f"i got: {data}")
+            check_turn_on_button(data)
         except KeyboardInterrupt:
             break
 
--- server_controlPC.py
+++ server_controlPC.py
@@ -8,28 +8,48 @@ BIND_IP = "0.0.0.0"
 PORT_KEYBOARD_SOCKET = 12346
 BUFFER_SIZE = 1024
 
+SIZE_HEADER_FORMAT = "0000000|" # n digits for data size + one delimiter
+size_header_size = len(SIZE_HEADER_FORMAT)
+TCP_DEBUG = True
+LEN_TO_PRINT = 100
+
+
+def send_with_size(sock, bdata):
+    if type(bdata) == str:
+        bdata = bdata.encode()
+    len_data = len(bdata)
+    header_data = str(len_data).zfill(size_header_size - 1) + "|"
+
+    bytea = bytearray(header_data,encoding='utf8') + bdata
+    sock.send(bytea)
+
+
+    if TCP_DEBUG and  len_data > 0:
+        print ("\nSent(%s)>>>" % (len_data,), end='')
+        print ("%s"%(bytea[:min(len(bytea),LEN_TO_PRINT)],))
 
 
 def send_button(button,sock):
-    sock.settimeout(1)
-    data = {
-        "key":button
-    }
+    # sock.settimeout(1)
+    # data = {
+    #     "key":button
+    # }
+    # json_message = json.dumps(data) + "\n"
+    # try:
+    #     if sock.recv(BUFFER_SIZE) == b"":
+    #         return
+    #
+    # except socket.error as err:
+    #     if err.errno == 10035 or str(err) == "timed out":
+    #         sock.sendall(json_message.encode('utf-8'))
+    #     elif err.errno == 10054:
+    #         print("Error %d Client is Gone.  reset by peer." % (err.errno))
+    #         return
+    #     else:
+    #         print("%d General Sock Error Client disconnected" % (err.errno))
+    #         return
 
-    json_message = json.dumps(data) + "\n"
-    try:
-        if sock.recv(BUFFER_SIZE) == b"":
-            return
-
-    except socket.error as err:
-        if err.errno == 10035 or str(err) == "timed out":
-            sock.sendall(json_message.encode('utf-8'))
-        elif err.errno == 10054:
-            print("Error %d Client is Gone.  reset by peer." % (err.errno))
-            return
-        else:
-            print("%d General Sock Error Client disconnected" % (err.errno))
-            return
+    send_with_size(sock,button)
 
 
 
@@ -48,7 +68,7 @@ def computer_keyboard_server():
 
     print(f"{address} connect with our keyboard socket")
 
-    keyboard.on_release(callback=lambda e: new_key(e,sock))
+    keyboard.on_press(callback=lambda e: send_button(e.name, sock))
     keyboard.wait()
 
     sock.close()
